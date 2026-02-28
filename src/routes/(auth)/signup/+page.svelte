b0ad08fdc28b4ab51cfa867bf386b9785b332741
JeongHeon <kdrlawjdgjs@gmail.com>
2026-02-28 21:35:16 +0900

    feat(signup): 회원가입 Form Action 및 페이지 stub 구현

---
+++ src/routes/(auth)/signup/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	let { form } = $props();
+</script>
+
+<form method="POST">
+	<input type="text" name="name" value={form?.name ?? ''} />
+	<input type="email" name="email" value={form?.email ?? ''} />
+	<input type="password" name="password" />
+	<button type="submit">회원가입</button>
+</form>
