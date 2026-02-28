46354d1e58e93459326770807dd8e5ba19d13568
JeongHeon <kdrlawjdgjs@gmail.com>
2026-02-28 21:35:09 +0900

    feat(login): 로그인 Form Action 및 페이지 stub 구현

---
+++ src/routes/(auth)/login/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	let { form } = $props();
+</script>
+
+<form method="POST">
+	<input type="email" name="email" value={form?.email ?? ''} />
+	<input type="password" name="password" />
+	<button type="submit">로그인</button>
+</form>
